058ea1e5402f73e962e6fb9020ebf8d219de299f
Avijeet Pandey <avijeet.pandey@jiostar.com>
2025-08-28 19:01:55 +0530

    Initial Commit

---
+++ Screenz/ScreenzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenzApp.swift
+//  Screenz
+//
+//  Created by Avijeet Pandey on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
